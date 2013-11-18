3c131b9edd2f27ce45512a25b9b4a31af77eb6e7
Sravan <sravan@Sravan.(none)>
2013-11-17 23:43:25 -0500

    upadted dispatcher file

--- src/main/java/com/sumadga/utils/ApplicationProperties.java
+++ src/main/java/com/sumadga/utils/ApplicationProperties.java
@@ -5,8 +5,50 @@ public class ApplicationProperties {
 	private Integer maxSubFolders;
 	private Boolean isVideoTranscodingEnabled;
 	private String videoTranscodingScriptPath;
-
+	private String qubeCellURL;
+	private String username;
+	private String password;
+	private String msisdnReturnURL;
+	private String eventChargeReturnURL;
+	private String secretKey;
 	
+	
+	public String getQubeCellURL() {
+		return qubeCellURL;
+	}
+	public void setQubeCellURL(String qubeCellURL) {
+		this.qubeCellURL = qubeCellURL;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMsisdnReturnURL() {
+		return msisdnReturnURL;
+	}
+	public void setMsisdnReturnURL(String msisdnReturnURL) {
+		this.msisdnReturnURL = msisdnReturnURL;
+	}
+	public String getEventChargeReturnURL() {
+		return eventChargeReturnURL;
+	}
+	public void setEventChargeReturnURL(String eventChargeReturnURL) {
+		this.eventChargeReturnURL = eventChargeReturnURL;
+	}
+	public String getSecretKey() {
+		return secretKey;
+	}
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
 	public String getMediaAbsolutePath() {
 		return mediaAbsolutePath;
 	}
